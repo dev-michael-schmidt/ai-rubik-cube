b9bbeb048d6f103357290e0ce5e95fc53b38055f
Michael Schmidt <Michael.Schmidt82@gmail.com>
2019-01-13 23:08:28 -0600

    for testing horizontal rotations

--- solve.py
+++ solve.py
@@ -2,3 +2,11 @@ from rubiks_cube import RubiksCube
 
 r = RubiksCube()
 print(r)
+print()
+
+#r.vertical_rotate('right', 'up')
+r.vertical_rotate('right', 'down')
+
+r.horizontal_rotate('middle', 'left')
+
+print(r)
