d49fab2c7ee849854a74bff5be0e9017d4c1cc79
Michael Schmidt <Michael.Schmidt82@gmail.com>
2019-01-14 23:00:45 -0600

    added z_rotation method

--- rubiks_cube.py
+++ rubiks_cube.py
@@ -1,4 +1,16 @@
 import numpy as np
+from termcolor import colored
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 class RubiksCube:
 
     def __init__(self):
@@ -51,26 +63,26 @@ class RubiksCube:
 
         return s
 
-    def vertical_rotate(self, slot, dir):
+    def y_rotate(self, slot, dir):
         """
 
-        :slot:  str     left, middle, right
+        :slot:  str     left, right
         :dir:   str     up, down
         """
 
-        if slot != 'left' and slot != 'middle' and slot != 'right':
+        if slot != 'left' and slot != 'right':
             raise
 
         if dir != 'up' and dir != 'down':
             raise
 
         temp = [] # IDEA: using np slicing below ??
-        c = 3 if slot == 'left' else 4 if slot == 'middle' else 5
+        c = 3 if slot == 'left' else 5
         anti_c = 14 - c
 
         # NOTE: probably a bug on rotations for right most group on 3x6 and 9x12 (np.rot90)??
 
-        temp = self.cube[r][c] for r in range(9)
+        temp = [self.cube[r][c] for r in range(9)]
         for r in range(3, 6):
             temp.append(self.cube[r][anti_c])
 
@@ -91,11 +103,11 @@ class RubiksCube:
             if dir == 'up':
                 self.cube[3:6, 6:9] = np.rot90(self.cube[3:6, 6:9], k=2)
 
-    def horizontal_rotate(self, slot, dir):
+    def x_rotate(self, slot, dir):
         """
 
         """
-        if slot != 'top' and slot != 'middle' and slot != 'bottom':
+        if slot != 'top' and slot != 'bottom':
             raise
 
         if dir != 'left' and dir != 'right':
@@ -103,6 +115,38 @@ class RubiksCube:
 
         r = 3 if slot == 'top' else 4 if slot == 'middle' else 5
         c = -3 if dir == 'right' else 3
-        temp = np.concatenate((self.cube[r, c:], self.cube[r, :c]))
+        self.cube[r, :] = np.concatenate((self.cube[r, c:], self.cube[r, :c]))
 
-        self.cube[r, :] = temp
+        if slot == 'top':
+            if dir == 'left':
+                self.cube[0:3, 3:6] = np.rot90(self.cube[0:3, 3:6], k=3)
+            else:
+                self.cube[0:3, 3:6] = np.rot90(self.cube[0:3, 3:6])
+        elif slot == 'bottom':
+            if dir == 'left':
+                self.cube[6:9, 3:6] = np.rot90(self.cube[6:9, 3:6])
+            else:
+                self.cube[6:9, 3:6] = np.rot90(self.cube[6:9, 3:6], k=3)
+
+    def z_rotate(self, slot, dir):
+        """
+
+        """
+        if slot != 'front' and slot != 'back':
+            raise
+
+        if dir != 'clockwise' and dir != 'anti-clockwise':
+            raise
+
+        edges = [
+            np.array(self.cube[2, 3:6]), np.array(self.cube[3:6, 6]),
+            np.array(self.cube[6, 3:6]), np.array(self.cube[3:6, 2])
+        ]
+
+        if slot == 'front':
+            if dir == 'clockwise':
+                self.cube[2, 3:6] = np.flip(edges[3])
+                self.cube[3:6, 6] = edges[0]
+                self.cube[6, 3:6] = np.flip(edges[1])
+                self.cube[3:6, 2] = edges[2]
+                self.cube[3:6, 3:6] = np.rot90(self.cube[3:6, 3:6], k=3)
