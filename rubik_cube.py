eee87728ed4ea5da06fce74a09b34c4109511672
Michael Schmidt <Michael.Schmidt82@gmail.com>
2019-01-27 16:56:50 -0600

    linting and testing a linter

--- rubik_cube.py
+++ rubik_cube.py
@@ -1,13 +1,16 @@
 import numpy as np
 
 class RubikCube:
+    """
+    Hello.  I am a docstring.
+    """
 
     def __init__(self):
         self.cube = [[' ' for _ in range(12)] for _ in range(9)]
         self.cube = np.array(self.cube)
-        for r in range(3):
-            for c in range(3, 6):
-                self.cube[r][c] = 'W'
+        for r_idx in range(3):
+            for c_idx in range(3, 6):
+                self.cube[r_idx][c_idx] = 'W'
 
         for r in range(3, 6):
             for c in range(3):
@@ -27,57 +30,57 @@ class RubikCube:
                 self.cube[r][c] = 'O'
 
     def __str__(self):
-        s = ''
-        for r in range(9):
-            row = self.cube[r][0]
-            for c in range(1, 12):
-                row = '{} {}'.format(row, self.cube[r][c])
+        res = ''
+        for r_idx in range(9):
+            row = self.cube[r_idx][0]
+            for c_idx in range(1, 12):
+                row = '{} {}'.format(row, self.cube[r_idx][c_idx])
 
-            if r == 0:
-                s = row
+            if r_idx:
+                res = '{}\n{}'.format(res, row)
             else:
-                s = '{}\n{}'.format(s, row)
+                res = row
 
-        return s
+        return res
 
-    def y_rotate(self, slot, dir):
+    def y_rotate(self, slot, direction):
         """
 
         :slot:  str     left, right
         :dir:   str     up, down
         """
 
-        if slot != 'left' and slot != 'right':
+        if slot not in ('left', 'right'):
             raise
 
-        if dir != 'up' and dir != 'down':
+        if direction not in ('up', 'down'):
             raise
 
         temp = [] # IDEA: using np slicing below ??
-        c = 3 if slot == 'left' else 5
-        anti_c = 14 - c
+        col = 3 if slot == 'left' else 5
+        anti_col = 14 - col
 
         # NOTE: probably a bug on rotations for right most group on 3x6 and 9x12 (np.rot90)??
 
-        temp = [self.cube[r][c] for r in range(9)]
-        for r in range(3, 6):
-            temp.append(self.cube[r][anti_c])
+        temp = [self.cube[r_idx][col] for r_idx in range(9)]
+        for r_idx in range(3, 6):
+            temp.append(self.cube[r_idx][anti_col])
 
-        temp = temp[3:] + temp[:3] if dir == 'up' else temp[-3:] + temp[:-3]
+        temp = temp[3:] + temp[:3] if direction == 'up' else temp[-3:] + temp[:-3]
 
-        for r in range(9):
-            self.cube[r][c] = temp[r]
+        for r_idx in range(9):
+            self.cube[r_idx][col] = temp[r_idx]
 
-        for r in range(3, 6):
-            self.cube[r][anti_c] = temp[r + 6]
+        for r_idx in range(3, 6):
+            self.cube[r_idx][anti_col] = temp[r_idx + 6]
 
         if slot == 'left':
             self.cube[3:6, 0:3] = np.rot90(self.cube[3:6, 0:3])
-            if dir == 'down':
+            if direction == 'down':
                 self.cube[3:6, 0:3] = np.rot90(self.cube[3:6, 0:3], k=2)
         elif slot == 'right':
             self.cube[3:6, 6:9] = np.rot90(self.cube[3:6, 6:9])
-            if dir == 'up':
+            if direction == 'up':
                 self.cube[3:6, 6:9] = np.rot90(self.cube[3:6, 6:9], k=2)
 
     def x_rotate(self, slot, dir):
